f55f70f592c73fc8d77b3521aa2a8c72728f52c7
Kyle Oliver <oliverke@mail.uc.edu>
2024-07-07 04:56:07 -0400

    Adds some additional unit tests.

--- tests/unit_tests/test_parametric.py
+++ tests/unit_tests/test_parametric.py
@@ -3,19 +3,37 @@ from __future__ import annotations
 from typing import Any
 from typing import Generator
 from typing import Iterable
+from typing import Literal
 from typing import TypeVar
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 from typing_extensions import ParamSpec
 
+from pytest_static.parametric import _iter_bool_instances
+from pytest_static.parametric import _iter_bytes_instances
+from pytest_static.parametric import _iter_complex_instances
+from pytest_static.parametric import _iter_float_instances
+from pytest_static.parametric import _iter_int_instances
+from pytest_static.parametric import _iter_literal_instances
+from pytest_static.parametric import _iter_none_instances
+from pytest_static.parametric import _iter_str_instances
 from pytest_static.parametric import get_all_possible_type_instances
 from pytest_static.parametric import iter_instances
 from pytest_static.parametric import type_handlers
+from pytest_static.type_sets import DEFAULT_INSTANCE_SETS
+from pytest_static.type_sets import INT_PARAMS
 from tests.util import BASIC_TYPE_EXPECTED_EXAMPLES
+from tests.util import BOOL_LEN
+from tests.util import BYTES_LEN
+from tests.util import COMPLEX_LEN
+from tests.util import FLOAT_LEN
+from tests.util import INT_LEN
+from tests.util import NONE_LEN
 from tests.util import PRODUCT_TYPE_EXPECTED_EXAMPLES
 from tests.util import PRODUCT_TYPE_MISSING_GENERIC_EXPECTED_EXAMPLES
 from tests.util import SPECIAL_TYPE_EXPECTED_EXAMPLES
+from tests.util import STR_LEN
 from tests.util import SUM_TYPE_EXPECTED_EXAMPLES
 
 
@@ -92,3 +110,42 @@ def test_iter_instances_with_missing_generic_type(typ: Any, expected_len: int) -
     assert expected_len == -1
     with pytest.raises(TypeError):
         assert [*iter_instances(typ)]
+
+
+@pytest.mark.parametrize(argnames="key", argvalues=DEFAULT_INSTANCE_SETS.keys())
+def test_type_handlers(key: Any) -> None:
+    from pytest_static.parametric import type_handlers as test_handlers
+
+    assert key in test_handlers._proxy
+
+
+def test__iter_none_instances() -> None:
+    assert_len(_iter_none_instances(None, ()), NONE_LEN)
+
+
+def test__iter_bool_instances() -> None:
+    assert_len(_iter_bool_instances(bool, ()), BOOL_LEN)
+
+
+def test__iter_int_instances() -> None:
+    assert_len(_iter_int_instances(int, ()), INT_LEN)
+
+
+def test__iter_float_instances() -> None:
+    assert_len(_iter_float_instances(float, ()), FLOAT_LEN)
+
+
+def test__iter_complex_instances() -> None:
+    assert_len(_iter_complex_instances(complex, ()), COMPLEX_LEN)
+
+
+def test__iter_str_instances() -> None:
+    assert_len(_iter_str_instances(str, ()), STR_LEN)
+
+
+def test__iter_bytes_instances() -> None:
+    assert_len(_iter_bytes_instances(bytes, ()), BYTES_LEN)
+
+
+def test__iter_literal_instances() -> None:
+    assert_len(_iter_literal_instances(Literal, tuple(INT_PARAMS)), INT_LEN)
